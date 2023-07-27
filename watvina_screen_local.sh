640a59b441fd47893233bba3598b53d3ad5d2f59
ChloePrice <70088757+cn-lugen@users.noreply.github.com>
2023-07-27 13:10:10 +0800

    Add files via upload

--- watvina_screen_local.sh
+++ watvina_screen_local.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-for f in mol/*.pdbqt; do
+for f in outpdbqt/*.pdbqt; do
     b=`basename $f .pdbqt`
 	echo $b
 	echo $f
     echo Processing ligand $b
     mkdir -p ./out/$b
-    ./watvina --config conf.txt --ligand $f  --out ./out/${b}/out.pdbqt --log ./out/${b}/log.txt
+    ./watvina --config conf.txt --implicitsol --ligand $f  --out ./out/${b}/${b}.pdbqt --log ./out/${b}/${b}.txt 
 done
